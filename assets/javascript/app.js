b39e97d53a90686b6c15fc185f79bb72ff2eb2ad
Umeadi, Marlon <umeadi@gmail.com>
2019-06-13 06:54:31 -0500

    altered HTML

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCTJNQM6jGJJEJpTWrwoXEVkm8S0i8WCa4",
+    authDomain: "train-scheduler-ac5c8.firebaseapp.com",
+    databaseURL: "https://train-scheduler-ac5c8.firebaseio.com",
+    projectId: "train-scheduler-ac5c8",
+    storageBucket: "train-scheduler-ac5c8.appspot.com",
+    messagingSenderId: "606506514161",
+    appId: "1:606506514161:web:a331d7f67b10bf43"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
